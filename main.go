a59cfc53ac049047cf469a5c5f710bfcdc12ab1a
David Gordon <davgordo@redhat.com>
2021-12-04 14:32:43 -0500

    LightningNode resource scaffolding

--- main.go
+++ main.go
@@ -85,6 +85,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "BitcoinNode")
 		os.Exit(1)
 	}
+	if err = (&controllers.LightningNodeReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "LightningNode")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
