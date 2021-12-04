c748d47d4f2cb0cf1f0c4ddd37944e439acf358e
David Gordon <davgordo@redhat.com>
2021-12-04 15:16:09 -0500

    add bitcoin connection params to LightningNode resource

--- api/v1alpha1/lightningnode_types.go
+++ api/v1alpha1/lightningnode_types.go
@@ -20,16 +20,20 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+type BitcoinConnection struct {
+	Host       string `json:"host,omitEmpty"`
+	Network    string `json:"network,omitEmpty"`
+	CertSecret string `json:"certSecret,omitempty"`
+	User       string `json:"user,omitempty"`
+	Password   string `json:"password,omitempty"`
+}
 
 // LightningNodeSpec defines the desired state of LightningNode
 type LightningNodeSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of LightningNode. Edit lightningnode_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	BitcoinNode BitcoinConnection `json:"bitcoinConnection,omitempty"`
 }
 
 // LightningNodeStatus defines the observed state of LightningNode
