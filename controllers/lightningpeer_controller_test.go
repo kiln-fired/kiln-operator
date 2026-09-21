09895f8c614d08115373660119f19a5cbfd65d39
David Gordon <davgordo@redhat.com>
2026-09-21 13:42:17 -0500

    Fix API-normalization test fixtures

--- controllers/lightningpeer_controller_test.go
+++ controllers/lightningpeer_controller_test.go
@@ -30,6 +30,16 @@ var _ = Describe("LightningPeer controller", func() {
 	createReadyNode := func() *bitcoinv1alpha1.LightningNode {
 		node := &bitcoinv1alpha1.LightningNode{
 			ObjectMeta: metav1.ObjectMeta{Name: nodeName, Namespace: namespace},
+			Spec: bitcoinv1alpha1.LightningNodeSpec{
+				BitcoinConnection: bitcoinv1alpha1.BitcoinConnection{External: &bitcoinv1alpha1.ExternalBitcoinConnection{
+					Host:                 "btcd",
+					Network:              "simnet",
+					CertSecret:           "btcd-rpc-tls",
+					ApiAuthSecretName:    "btcd-rpc-creds",
+					ApiUserSecretKey:     "username",
+					ApiPasswordSecretKey: "password",
+				}},
+			},
 		}
 		Expect(k8sClient.Create(ctx, node)).To(Succeed())
 		node.Status.Phase = "Ready"
